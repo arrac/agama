78a2c6dc9f224fd0aec34e1a0eabef3ba91e851e
Aditya Rachakonda <aditya.rachakonda@gmail.com>
2011-05-26 21:47:35 +0530

    Fixed Loader to load directed edges

--- examples/load_data.rb
+++ examples/load_data.rb
@@ -1,6 +1,6 @@
 require 'rubygems'
-require '../lib/agama'
-require '../lib/agama/adapters/tokyocabinet'
+require 'agama'
+require 'agama/adapters/tokyocabinet'
 require 'pp'
 
 g = Agama::Loader.new(:path => "/tmp", 
@@ -8,19 +8,20 @@ g = Agama::Loader.new(:path => "/tmp",
 
 g.open
                       
-g.load_nodes("/tmp/n") do |cols, hash|
+g.load_nodes("data/nodes") do |cols, hash|
   hash[:name]   = cols[0]
   hash[:weight] = cols[1]
 end
 
-g.load_edges("/tmp/e") do |cols, hash|
-  pp cols
+g.load_edges("data/bulk_edges") do |cols, hash|
+  #pp cols
   hash[:from]         = Hash.new
   hash[:to]           = Hash.new
   hash[:directed]     = false
   hash[:from][:name]  = cols[0]
   hash[:to][:name]    = cols[1]
-  hash[:weight]       = cols[2]
+  hash[:direction]    = cols[2]
+  hash[:weight]       = cols[3]
 end
 
 g.close
--- lib/agama/loader.rb
+++ lib/agama/loader.rb
@@ -35,14 +35,33 @@ module Agama
         cols = l.chomp.split("\t")
         edge = Hash.new
         yield cols, edge
-        if edge.size > 0 and edge[:from][:name] and edge[:to][:name]
+        if edge.size > 0 and edge[:from][:name] and edge[:to][:name] 
           type = edge[:type] || Config::DEFAULT_TYPE
           edge[:type] = type
+          raise "Unknown edge direction" unless (["I", "O", "N"].include?(edge[:direction]))
+          rev = false
+          if edge[:direction] == "I"
+            tmp         = edge[:from]
+            edge[:from] = edge[:to]
+            edge[:to]   = tmp
+            rev = true 
+          end
           
-          key, rk = Keyify.edge(edge)
-          value   = JSON.generate(Keyify.clean_edge(edge)) #remove key items from value
+          if ["I", "O"].include? edge[:direction]
+            edge[:directed] = true
+          else
+            edge[:directed] = false            
+          end
+          edge.delete(:direction)
+          
+          key, rev_key  = Keyify.edge(edge)
+          value         = JSON.generate(Keyify.clean_edge(edge)) #remove key items from value
 
-          @db.e_put(key, value)
+          if rev
+            @db.e_put(rev_key, value)
+          else
+            @db.e_put(key, value)
+          end
         end
       end
     end
