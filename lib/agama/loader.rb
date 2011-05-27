81e51cb9cea83bcdbc45fe9a7302dacd48a91dce
Aditya Rachakonda <aditya.rachakonda@gmail.com>
2011-05-27 14:52:43 +0530

    Bulk loading will now update the meta values correctly.

--- lib/agama/loader.rb
+++ lib/agama/loader.rb
@@ -3,6 +3,7 @@ module Agama
     def initialize(params)
       @db_path  = params[:path] || "./"
       @db       = params[:db]
+      @meta     = Hash.new
     end
 
     def open
@@ -10,6 +11,10 @@ module Agama
     end
 
     def close
+      @meta.each do |k, v|
+        @db.m_put(k, v.to_s)
+      end
+      
       @db.close
     end
     
@@ -24,6 +29,13 @@ module Agama
           
           key = Keyify.node(node)
           value = JSON.generate(Keyify.clean_node(node)) #remove key items from value
+          
+          #Update meta values
+          @meta["n"] = @db.m_get("n") || "0" unless @meta["n"]
+          @meta["n"] = @meta["n"].to_i + 1
+
+          @meta["node#{type}"] = @db.m_get("node#{type}") || "0" unless @meta["node#{type}"]
+          @meta["node#{type}"] = @meta["node#{type}"].to_i + 1
 
           @db.n_put(key, value)
         end
@@ -56,6 +68,14 @@ module Agama
           
           key, rev_key  = Keyify.edge(edge)
           value         = JSON.generate(Keyify.clean_edge(edge)) #remove key items from value
+
+          #Update meta values
+          @meta["m"] = @db.m_get("m") || "0" unless @meta["m"]
+          @meta["m"] = @meta["m"].to_i + 1
+
+          @meta["edge#{type}"] = @db.m_get("edge#{type}") || "0" unless @meta["edge#{type}"]
+          @meta["edge#{type}"] = @meta["edge#{type}"].to_i + 1
+
 
           if rev
             @db.e_put(rev_key, value)
